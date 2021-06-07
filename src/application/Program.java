1099d40b35d03d089a1feb6e6f5c7b8055d95496
lucashgp-dev <lucashgp.l@hotmail.com>
2021-06-07 18:41:55 -0300

    Placing pieces on the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import javax.swing.text.Position;
+
 import chess.ChessMatch;
 
 public class Program {
@@ -8,6 +10,6 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
 
         UI.printBoard(chessMatch.getPieces());
-        
+
     }
 }
