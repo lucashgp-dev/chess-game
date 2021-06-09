9ab0d5308f2473246dbbfe2b75cb82d401291cd5
lucashgp-dev <lucashgp.l@hotmail.com>
2021-06-09 10:17:18 -0300

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -70,17 +70,24 @@ public class UI {
 
     }
 
-    public static void printMatch(ChessMatch ChessMatch,List<ChessPiece> captured) {
-        printBoard(ChessMatch.getPieces());
+    public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
+        printBoard(chessMatch.getPieces());
         System.out.println();
         printCapturedPieces(captured);
         System.out.println();
-        System.out.println("Turn: " + ChessMatch.getTurn());
-        System.out.println("Waiting player: " + ChessMatch.getCurrentPlayer());
+        System.out.println("Turn: " + chessMatch.getTurn());
 
-        if(ChessMatch.getCheck()){
-            System.out.println("CHECK!");
+        if (!chessMatch.getCheckMate()) {
+            System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+
+            if (chessMatch.getCheck()) {
+                System.out.println("CHECK!");
+            }
+        } else {
+            System.out.println("CHECKMATE!");
+            System.out.println("Winner: " + chessMatch.getCurrentPlayer());
         }
+
     }
 
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
@@ -117,8 +124,10 @@ public class UI {
     }
 
     private static void printCapturedPieces(List<ChessPiece> captured) {
-        List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
-        List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+        List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE)
+                .collect(Collectors.toList());
+        List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK)
+                .collect(Collectors.toList());
 
         System.out.println("Captured pieces:");
         System.out.print("White: ");
